0f7b202da05f220ac8888ff5f335ead636332dfb
VasGorn <vas.goroshko@gmail.com>
2021-06-06 20:09:42 +0300

    get data from upload request

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,15 @@
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -25,8 +29,26 @@ public class Controller extends HttpServlet {
 		System.out.println("ACTION: " + action);
 		if (action != null) {
 			switch (action) {
-				default:
-					break;
+			case "upload":
+				url = base + "optimization.jsp";
+				String sStepTime = request.getParameter("stepTime");
+				double stepTimeSize = Double.parseDouble(sStepTime);
+				
+				String sStepFunctionValue = request.getParameter("stepFunctionValue");
+				double stepFunctionValue = Double.parseDouble(sStepFunctionValue);
+				
+				System.out.println("Step time: " + request.getParameter("stepTime"));
+				System.out.println("Step function value: " + request.getParameter("stepFunctionValue"));
+				
+				Part filePart = request.getPart("csvFile");
+				InputStream fileContent = filePart.getInputStream();
+
+				Reader in = new InputStreamReader(fileContent);
+
+
+				break;
+			default:
+				break;
 			}
 		}
 		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(url);
