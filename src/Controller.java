8152b05d001b1ce723e8eeea0307cba9710b90ad
VasGorn <vas.goroshko@gmail.com>
2021-06-07 20:13:57 +0300

    upload csv file to reference function object

--- src/Controller.java
+++ src/Controller.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import com.opencsv.CSVReader;
+
+import beans.RefFunction;
+import utils.StrToNumber;
 
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -44,7 +52,34 @@ public class Controller extends HttpServlet {
 				InputStream fileContent = filePart.getInputStream();
 
 				Reader in = new InputStreamReader(fileContent);
+				CSVReader reader;
+				Iterator<String[]> iterator;
 
+				try {
+					reader = new CSVReader(in);
+					iterator = reader.iterator();
+					
+					List<Double> lyData = new LinkedList<>();
+					String[] row;
+					while(iterator.hasNext()) {
+						row = iterator.next();
+						String sItem = row[0];
+						double item = StrToNumber.getDouble(sItem);
+						lyData.add(item);
+						System.out.println("ITEM: " + item);
+					}
+					
+					RefFunction refFunction = new RefFunction(lyData, stepFunctionValue, stepTimeSize);
+					
+					reader.close();
+					
+				} catch (Exception e) {
+					System.out.println("Error in CSVReader: " + e.toString());
+				} finally {
+					if(in != null) {
+						in.close();
+					}
+				}
 
 				break;
 			default:
