bc6ef6343ea45811292595e21b519ff292bcded3
VasGorn <vas.goroshko@gmail.com>
2021-07-16 20:50:37 +0300

    create tournament selection

---
+++ src/algorithm/genetic/SelectionTournamentImp.java
@@ -0,0 +1,26 @@
+package algorithm.genetic;
+
+import interfaces.ISelection;
+
+public class SelectionTournamentImp implements ISelection{
+	private int tournamentSize;
+	
+	public SelectionTournamentImp(int tournamentSize) {
+		this.tournamentSize = tournamentSize;
+	}
+
+	public Population getParentPopulation(Population pop) {
+		Population parentPop = new Population(pop.size(),false);
+		for(int j = 0; j < pop.size(); ++j) {
+			Population tournament = new Population(this.tournamentSize, false);
+			for(int i = 0; i < this.tournamentSize; ++i) {
+				int randomIndivIndex = (int) (Math.random() * pop.size());
+				tournament.saveIndividual(i, pop.getIndividual(randomIndivIndex));
+			}
+			Individual fittest = tournament.getFittest();
+			parentPop.saveIndividual(j, fittest);
+		}
+		return parentPop;
+	}
+	
+}
